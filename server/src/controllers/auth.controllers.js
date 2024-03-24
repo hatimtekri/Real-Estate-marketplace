5a2830b62331bedb2fbd6e9a5a615f9916354aa4
hatimtekri <hatimtekri53@gmail.com>
2024-03-24 23:34:35 +0530

    sign in api route

--- server/src/controllers/auth.controllers.js
+++ server/src/controllers/auth.controllers.js
@@ -1,12 +1,34 @@
 import { User } from "../models/user.model.js";
 import { asyncErrorHandler } from "../utils/asyncErrorHandler.js";
-import bcrypt from 'bcryptjs'
+import bcrypt from "bcryptjs";
+import { ApiError } from "../utils/ApiError.js";
+import jwt from 'jsonwebtoken'
+import { json } from "express";
 
 export const signup = asyncErrorHandler(async (req, res) => {
   const { username, email, password } = req.body;
-  const hashedPassword = bcrypt.hashSync(password,10)
-  const newUser = new User({ username, email, password:hashedPassword });
-  await newUser.save()
-  console.log(username,email,password);
+  const hashedPassword = bcrypt.hashSync(password, 10);
+  const newUser = new User({ username, email, password: hashedPassword });
+  await newUser.save();
+  console.log(username, email, password);
   res.status(201).json("User created successfully");
+});
+
+export const signin = asyncErrorHandler(async (req, res) => {
+  const { email, password } = req.body;
+  const validUser = await User.findOne({ email: email });
+
+  if (!validUser) {
+    throw new ApiError(400, "Invalid credentails");
+  }
+  const validPassword = bcrypt.compareSync(password,validUser.password)
+
+  if(!validPassword) {
+    throw new ApiError(401,'invalid password')
+  }
+  const token = jwt.sign({id:validUser._id},process.env.JWT_SECRET)
+
+
+  res.cookie("access_token",token,{httpOnly:true}).status(200).json(validUser)
+
 });
--- server/src/routes/auth.route.js
+++ server/src/routes/auth.route.js
@@ -1,9 +1,11 @@
 import express from 'express'
-import { signup } from '../controllers/auth.controllers.js';
+import { signin, signup } from '../controllers/auth.controllers.js';
 
 const router = express.Router();
 
 router.route('/signup').post(signup);
+
+router.route('/signin').post(signin)
 
 
 export default router
